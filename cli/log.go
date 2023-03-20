b1252f5408a614cfb2e537847bbf88137653bade
Douglas G. Moore <doug@39alpharesearch.org>
2023-03-20 01:05:47 -0700

    Update log entry formatting

--- cli/log.go
+++ cli/log.go
@@ -2,6 +2,8 @@ package cli
 
 import (
 	"fmt"
+	"strings"
+	"text/tabwriter"
 
 	"github.com/39alpha/dorthy/core"
 )
@@ -13,15 +15,31 @@ func Log() error {
 	}
 
 	for i := len(manifest) - 1; i >= 0; i-- {
-		printCommit(manifest[i])
+		commit := manifest[i]
+		showParents := len(commit.Parents) >= 1 && i != 0 && commit.Parents[0] != manifest[i-1].Hash
+		printCommit(manifest[i], showParents)
 	}
 
 	return nil
 }
 
-func printCommit(commit core.Commit) {
-	fmt.Printf("hash:   %v\n", commit.Hash)
-	fmt.Printf("Author: %v\n", commit.Author)
-	fmt.Printf("Date:   %s\n\n", commit.Date.Format("Mon Jan 02 15:04:05 2006 -0700"))
-	fmt.Printf("    %s\n\n", commit.Message)
+func printCommit(commit core.Commit, showParents bool) {
+	s := strings.Builder{}
+	t := tabwriter.NewWriter(&s, 0, 4, 2, ' ', 0)
+	fmt.Fprintf(t, "%s\t%s\n", "Hash:", commit.Hash)
+	fmt.Fprintf(t, "%s\t%s\n", "Author:", commit.Author)
+	fmt.Fprintf(t, "%s\t%s\n", "Date:", commit.Date.Format("Mon Jan 02 15:04:05 2006 -0700"))
+	fmt.Fprintf(t, "%s\t%s\n", "Type:", core.PathTypeString(commit.Type))
+	if showParents {
+		for i, parent := range commit.Parents {
+			if i == 0 {
+				fmt.Fprintf(t, "%s\t%s\n", "Parents:", parent)
+			} else {
+				fmt.Fprintf(t, "%s\t%s\n", "", parent)
+			}
+		}
+	}
+	t.Flush()
+
+	fmt.Printf("%s\n    %s\n\n", s.String(), commit.Message)
 }
