6f07ae22c776f257373ed9fafe72b19944a498f9
Douglas G. Moore <doug@39alpharesearch.org>
2025-03-31 10:33:07 -0700

    Remove teams from homepage

--- dataforge/handlers/home.go
+++ dataforge/handlers/home.go
@@ -9,14 +9,12 @@ type Home struct {
 	ErrorHandler
 
 	user     *models.User
-	teams    []models.Team
 	datasets []models.Dataset
 }
 
 func (page *Home) Pre(c *fiber.Ctx) error {
 	page.user, _ = c.Locals("AuthUser").(*models.User)
 
-	page.teams, _ = page.DB().GetHotTeams(page.user)
 	page.datasets, _ = page.DB().GetHotDatasets(page.user)
 
 	return nil
@@ -25,7 +23,6 @@ func (page *Home) Pre(c *fiber.Ctx) error {
 func (page *Home) RenderHtml(c *fiber.Ctx) error {
 	return c.Status(fiber.StatusOK).Render("index", Bind(c, fiber.Map{
 		"AuthUser": page.user,
-		"Teams":    page.teams,
 		"Datasets": page.datasets,
 	}), "layouts/main")
 }
@@ -33,7 +30,6 @@ func (page *Home) RenderHtml(c *fiber.Ctx) error {
 func (page *Home) RenderJson(c *fiber.Ctx) error {
 	return c.Status(fiber.StatusOK).JSON(fiber.Map{
 		"AuthUser": page.user,
-		"Teams":    page.teams,
 		"Datasets": page.datasets,
 	})
 }
